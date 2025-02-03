b33c707e0f558b7e24d415951fdab84e1b23366b
ingun <43471475+parkingun@users.noreply.github.com>
2025-02-03 12:29:51 +0900

    Feat: ingun test (#12)

--- src/pages/sample/components/Geon.tsx
+++ src/pages/sample/components/Geon.tsx
@@ -1,7 +1,7 @@
 import style from "./Geon.module.css";
 
 const Geon = () => {
-  return <div className={style.wrapper}>인건님 컴포넌트</div>;
+  return <div className={style.wrapper}>인건님 컴포넌트 - 테스트</div>;
 };
 
 export default Geon;
