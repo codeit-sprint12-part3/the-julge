f36b3e2c4cf2a1640c0dbeed309b31d30b0b5b3f
kss761036 <kss761036@gmail.com>
2025-02-03 12:31:02 +0900

    feat: tset (#14)
    Co-authored-by: 김승석 <kss@gimseungseog-ui-MacBookAir.local>

--- src/pages/sample/components/Seung.tsx
+++ src/pages/sample/components/Seung.tsx
@@ -1,7 +1,7 @@
 import style from "./Seung.module.css";
 
 const Seung = () => {
-  return <div className={style.wrapper}>승석님 컴포넌트</div>;
+  return <div className={style.wrapper}>승석님 컴포넌트 테스트</div>;
 };
 
 export default Seung;
