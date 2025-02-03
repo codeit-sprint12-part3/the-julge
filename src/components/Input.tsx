34ccef900208173fca201a4f13f9dc3577f9253a
parksangseok <>
2025-02-03 18:39:20 +0900

    Refactor: 1.기본폰트 색상 적용 2. 기본폰트 1.6rem 변경

---
+++ src/components/Input.tsx
@@ -0,0 +1,55 @@
+import style from "./Input.module.css";
+
+interface InputProps {
+  placeholder?: string,
+  type: "text" | "password" | "email" | "number";
+  name?: string,
+  value?: string,
+  id?: string,
+  label?: string,
+  className?: string,
+  required?: boolean,
+  error?: boolean;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+const Input = ({
+  placeholder,
+  type,
+  value,
+  id,
+  name,
+  label,
+  className,
+  required,
+  error,
+  onChange }: InputProps) => {
+
+  return (
+    <>
+      {
+        label &&
+        <label htmlFor={id} className={`${style.label} ${className}`}>
+          {label}
+          {required && <span className={style.required}>*</span>}
+        </label>
+      }
+      <input
+        id={id}
+        className={`${style.input} ${className} ${error ? style.errorInput : ""}`}
+        placeholder={placeholder}
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+      {
+        error && <div className={style.errorMessage}>
+          {error}
+        </div>
+      }
+    </>
+  );
+};
+
+export default Input;
