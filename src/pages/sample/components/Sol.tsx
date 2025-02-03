d06af69af414ea75edb40619514abe6fea8dab02
HANSOL <121593205+sol322@users.noreply.github.com>
2025-02-03 12:29:41 +0900

    feat: add input (#13)

--- src/pages/sample/components/Sol.tsx
+++ src/pages/sample/components/Sol.tsx
@@ -1,7 +1,7 @@
 import style from "./Sol.module.css";
 
 const Sol = () => {
-  return <div className={style.wrapper}>한솔님 컴포넌트 - 테이블 작업</div>;
+  return <div className={style.wrapper}>한솔님 컴포넌트 - 깃 플로우 테스트</div>;
 };
 
 export default Sol;
