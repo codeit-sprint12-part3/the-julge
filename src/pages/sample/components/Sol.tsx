14474cdec072cb90d5a0d138f91a15e607f144ab
HANSOL <121593205+sol322@users.noreply.github.com>
2025-02-03 11:19:54 +0900

    test

--- src/pages/sample/components/Sol.tsx
+++ src/pages/sample/components/Sol.tsx
@@ -1,7 +1,7 @@
 import style from "./Sol.module.css";
 
 const Sol = () => {
-  return <div className={style.wrapper}>한솔님 컴포넌트</div>;
+  return <div className={style.wrapper}>한솔님 컴포넌트 - 수정 테스트</div>;
 };
 
 export default Sol;
