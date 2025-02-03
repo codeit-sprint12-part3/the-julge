42ef792fa2acc1b5d1e1322d14ca9fe8fb0097ef
parksangseok <>
2025-02-03 12:24:31 +0900

    feat: 테스트 완료

--- src/pages/sample/components/Sang.tsx
+++ src/pages/sample/components/Sang.tsx
@@ -1,7 +1,7 @@
 import style from "./Sang.module.css";
 
 const Sang = () => {
-  return <div className={style.wrapper}>상석님 컴포넌트</div>;
+  return <div className={style.wrapper}>상석님 컴포넌트 테스트</div>;
 };
 
 export default Sang;
