a0d575ef5ff9f4437d310f55ddee133ad3a18189
HANSOL <121593205+sol322@users.noreply.github.com>
2025-02-05 14:12:40 +0900

    #49 feat : 공용 컴포넌트 아이콘 & 로고 개발 (#52)
    * Feat: icon components
    
    * Feat: logo components

---
+++ src/components/icon/Icon.tsx
@@ -0,0 +1,31 @@
+import { colorMap } from "./iconColor";
+import { iconData } from "./iconData";
+
+type IconProps = {
+  name: keyof typeof iconData;
+  size?: number;
+  color?: keyof typeof colorMap | string;
+} & React.SVGProps<SVGSVGElement>;
+
+export function Icon({ name, size = 24, color, ...props }: IconProps) {
+  const icon = iconData[name];
+  if (!icon) {
+    console.warn(`${name}은(는) 없는 아이콘입니다.`);
+    return null;
+  }
+
+  const targetColor = color ? colorMap[color as keyof typeof colorMap] || color : "currentColor";
+
+  return (
+    <svg
+      width={size}
+      height={size}
+      fill={targetColor}
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      {icon}
+    </svg>
+  );
+}
--- src/pages/sample/components/Sol.tsx
+++ src/pages/sample/components/Sol.tsx
@@ -1,13 +1,59 @@
 import Alerts from "@/components/alerts/Alerts";
-import styles from "./Sol.module.css";
+import style from "./Sol.module.css";
+import { Icon } from "@/components/icon/Icon";
+import Logo from "@/components/ui/Logo";
 
 const Sol = () => {
   return (
-    <div>
-      <div>
-        <h4 className={styles.title}>[알림창 - Alerts]</h4>
-        <Alerts />
-      </div>
+    <div className={style.wrapper}>
+      <ul>
+        <li>
+          <h3>Alerts</h3>
+          <div className={style.box}>
+            <Alerts />
+          </div>
+        </li>
+        <li>
+          <h3>Logo</h3>
+          <div>
+            <p>1️⃣기본 : {`<Logo />`}</p>
+            <p>2️⃣사이즈 : {`<width={248} height={45} />`}</p>
+          </div>
+          <div className={style.box}>
+            <Logo />
+          </div>
+        </li>
+        <li>
+          <h3>Icon</h3>
+          <div>
+            <p>1️⃣아이콘명 : {`<Icon name="camera" />`}</p>
+            <p>2️⃣사이즈 : {`<size=16 />`}</p>
+            <p>
+              3️⃣색상 : {`<color="red-400" />`}, {`<color="#e7e7e7" />`} → 색상 변수, 색상 코드 둘 다
+              사용 가능
+            </p>
+          </div>
+          <div className={style.box}>
+            <Icon name="alter" size={24} />
+            <Icon name="camera" />
+            <Icon name="check" />
+            <Icon name="warn" />
+            <Icon name="map" />
+            <Icon name="time" />
+            <Icon name="phone" />
+            <Icon name="mail" />
+            <Icon name="facebook" />
+            <Icon name="instargram" />
+            <Icon name="search" />
+            <Icon name="close" />
+            <Icon name="selectUp" />
+            <Icon name="selectDown" />
+            <Icon name="leftArr" />
+            <Icon name="rightArr" />
+            <Icon name="upArr" />
+          </div>
+        </li>
+      </ul>
     </div>
   );
 };
