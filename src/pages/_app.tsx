1937260e843b3e519badcdd0bafacb05fd7f7480
kss761036 <kss761036@gmail.com>
2025-02-06 10:09:11 +0900

    #66 feat: 공용 컴포넌트 toast 작업 (#67)
    * feat: create common component - table
    
    * feat: create common component - toast
    
    ---------
    
    Co-authored-by: 김승석 <kss@gimseungseog-ui-MacBookAir.local>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,12 @@ import "@/styles/globals.css";
 import { ModalProvider } from "@/context/ModalContext";
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
-
+import { Slide, toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "@/styles/Toast.css";
 import type { AppProps } from "next/app";
+
+export { toast };
 
 export default function App({ Component, pageProps }: AppProps) {
   const is404 = Component.displayName === "Error404Page";
@@ -18,6 +22,19 @@ export default function App({ Component, pageProps }: AppProps) {
         </main>
         {!is404 && <Footer />}
       </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Slide}
+      />
     </ModalProvider>
   );
 }
