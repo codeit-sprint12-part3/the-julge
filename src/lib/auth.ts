1fc4c2f572689d36b233f56a5e0c393553eea7d8
HANSOL <121593205+sol322@users.noreply.github.com>
2025-02-08 10:10:20 +0900

    Feat: add jwt-decode

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,10 @@
 import { client } from "@/lib/axiosClient";
+import { jwtDecode } from "jwt-decode";
+
+interface DecodedToken {
+  userId: string; // 사용자 ID
+  iat: number; // 발급 시간
+}
 
 export const login = async (email: string, password: string) => {
   const res = await client.post("/token", { email, password });
@@ -6,7 +12,8 @@ export const login = async (email: string, password: string) => {
 };
 
 export const fetchUserInfo = async (token: string) => {
-  const userId = JSON.parse(atob(token.split(".")[1])).userId; // JWT 디코딩
-  const res = await client.get(`/users/${userId}`);
+  // const userId = JSON.parse(atob(token.split(".")[1])).userId;
+  const decoded: DecodedToken = jwtDecode(token); // JWT 디코딩
+  const res = await client.get(`/users/${decoded.userId}`);
   return res.data.item;
 };
