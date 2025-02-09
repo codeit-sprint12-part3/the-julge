8c5adb31794d9893bf9d5f5b6139046a30931b4c
HANSOL <121593205+sol322@users.noreply.github.com>
2025-02-09 21:53:35 +0900

    Feat: EmptyState component

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,7 +1,23 @@
 import AuthGuard from "@/components/auth/AuthGuard";
+import EmptyState from "@/components/ui/EmptyState";
+import Title from "@/components/ui/Title";
 
 function Page() {
-  return <div className="">내 프로필 페이지</div>;
+  return (
+    <>
+      <section>
+        <Title text="내 프로필" />
+        <EmptyState
+          message={"내 프로필을 등록하고 원하는 가게에 지원해 보세요."}
+          buttonText={"내 프로필 등록하기"}
+          href={"/user/write"}
+        />
+      </section>
+      <section>
+        <Title text="신청 내역" />
+      </section>
+    </>
+  );
 }
 
 export default AuthGuard(Page, "employee");
