465ef20ac924ec3d9a862a230e6347ad61e0a388
parksangseok <park0157@naver.com>
2025-02-18 08:55:28 +0900

    Fix: 내가게 등록 선택박스 옵션 클릭시 유효성검사 되는 문제 해결, 무한스크롤 완료료

--- src/components/ui/SpinnerLoader.tsx
+++ src/components/ui/SpinnerLoader.tsx
@@ -1,9 +1,13 @@
 import style from "./SpinnerLoader.module.css";
 import Image from "next/image";
 
-const SpinnerLoader = () => {
+interface SpinnerLoaderProps {
+  className?: string;
+}
+
+const SpinnerLoader = ({ className }: SpinnerLoaderProps) => {
   return (
-    <div className={style.spinnerWrapper}>
+    <div className={`${style.spinnerWrapper} ${className || ""}`}>
       <Image src="/spinners.svg" alt="로딩 아이콘" width={100} height={100} />
     </div>
   );
